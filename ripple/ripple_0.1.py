a4325879f160601dd282a0c5567085e2a386ee67
Adam Hawkey <atomahoke@gmail.com>
2020-11-10 01:58:40 -0800

    Tested some of the variables and strings.  Need to map tape or clip name to clip.nameZZ

--- ripple/ripple_0.1.py
+++ ripple/ripple_0.1.py
@@ -12,6 +12,9 @@ EDIT_RATE = 24
 HOUR = 3600 * EDIT_RATE
 timeline = otio.adapters.read_from_file(inputEDL)
 for clip in timeline.each_clip():
+    # edl_meta = clip.metadata.get('cmx_3600', {})
+    # comments = edl_meta.get('comments', [])
+    
     ripple = -HOUR + 100
 
     start_frame = clip.source_range.start_time.value
@@ -21,11 +24,14 @@ for clip in timeline.each_clip():
         ripple = 100
 
     end_frame = start_frame + clip.source_range.duration.value + ripple
-
+    
     clip.source_range = otio.opentime.range_from_start_end_time(
         otio.opentime.from_frames(start_frame + ripple, EDIT_RATE),
         otio.opentime.from_frames(end_frame, EDIT_RATE)
     )
-
+    # print(clip.name, clip.source_range, clip.metadata)
+    # I need to map the clip or tape name to the clip.name
+    # print(start_frame + ripple, end_frame)
+# print(otio.adapters.write_to_string(timeline, adapter_name='cmx_3600', style='nucoda'))
 otio.adapters.write_to_file(timeline, outputEDL, adapter_name='cmx_3600', style='nucoda')
 
