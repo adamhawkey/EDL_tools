cc099ecfc50201f1b3dce2eb39f40070d3c5800d
adamhawkey <atomahoke@gmail.com>
2020-12-02 17:50:19 -0800

    changed some comments

--- Marker_tools/AvidMarkerList_to_Nucoda.py
+++ Marker_tools/AvidMarkerList_to_Nucoda.py
@@ -21,19 +21,10 @@ with open(marker_list, newline = '') as markers:
         #print(name, timecode, layer, color, comment)
         print("* LOC:", timecode, color,' ', comment) #need to figure out the timeline vs. segment locator in Nucoda.
 
-'''  # potentially, markers could be at the end of the EDL in the following structure...  Need to test in Nucoda.
-* ============================================================
-* Marker Metadata
-* ------------------------------------------------------------
-* 
-* Sequence name: KMP-4K_and_CTM_Test_DNxHR444
-* Number  Color   Marker Name              Start TC    End TC      Duration    Track  Part        Comment
-*      1  Red     GTS3_Dylan               01:00:25:10                         V1                 This is a marker
-*      2  Red     GTS3_Dylan               01:01:24:01                         V1                 This is also a marker
-*      3  Red     GTS3_Dylan               01:02:44:09                         V1                 This is still a marker
-*      4  Red     GTS3_Dylan               01:03:14:01                         V1                 This is a marker indicating where GH5 footage starts
 '''
-'''
+# Unfortunately, Nucoda doesn't support markers at the end of an EDL, like some others.
+# Turns out it reads them perfectly from before the first event as * LOC: style markers.  See README.md
+
 # other methods I tried
 lines = tuple(open(marker_list))
 for line in lines:
