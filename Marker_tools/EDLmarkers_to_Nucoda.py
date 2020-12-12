b0cae0ff8ce218541f21d093fae5135a08cc43a3
Adam Hawkey <atomahoke@gmail.com>
2020-12-11 21:37:48 -0800

    Added EDLmarkers_to_Nucoda.py to make a simple Locator only EDL for Nucoda

---
+++ Marker_tools/EDLmarkers_to_Nucoda.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+version: AvidMarkerList_to_Nucoda.py
+"""
+# This works for importing an Avid Marker List (text file), tab delimited,
+# as a single event EDL, with markers preceding it.  Import and select 
+# Import Bookmarks from EDL.
+
+# import opentimelineio as otio
+import opentimelineio as otio
+import sys
+import csv
+import re
+
+print(sys.argv[0])
+inputEDL, outputEDL = sys.argv[1:]
+
+header1 = ('TITLE: (Markers grabbed from) {} \n').format(inputEDL)
+header2 = ('FCM: FILM \n')
+footer1 = ('001  Bars   V     C        00:58:00:00 00:58:30:00 00:58:00:00 00:58:30:00\n')
+footer2 = ('* LOC: 00:58:20:00 green   Dummy clip that can be deleted\n')
+
+EDIT_RATE = 24
+HOUR = 3600 * EDIT_RATE
+timeline = otio.adapters.read_from_file(inputEDL, ignore_timecode_mismatch=True)
+# ignore_timecode_mismatch=True deals with the source timecode and duration 
+# not matching the timebase.
+
+markerwriter = open(outputEDL, 'w')
+markerwriter.write(header1)
+markerwriter.write(header2)
+
+with open(inputEDL, newline = '') as openEDL:
+    #lines = csv.reader(markers, delimiter='\t')
+    for line in openEDL:
+        if '* LOC:' in line:
+            print(line)
+#            name, timecode, layer, color, comment = line[:5]
+#            print('* LOC: {0} {1}   {2}'.format(timecode, color, comment))
+#            edl_line = ('* LOC: {0} {1}   {2}\n'.format(timecode, color, comment))
+            markerwriter.write(line)
+
+markerwriter.write(footer1)
+markerwriter.write(footer2)
+markerwriter.close()
