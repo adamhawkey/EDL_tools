25411cdda2f57f922273a5d84671414f8005697b
Adam Hawkey <atomahoke@gmail.com>
2020-12-28 17:34:51 -0800

    removed unused garbage

--- Marker_tools/EDLmarkers_to_Nucoda.py
+++ Marker_tools/EDLmarkers_to_Nucoda.py
@@ -7,7 +7,6 @@ version: EDLMarkers_to_Nucoda.py
 # as a single event EDL, with markers preceding it.  Import and select 
 # Import Bookmarks from EDL.
 
-# import opentimelineio as otio
 import sys
 import csv
 import re
@@ -20,22 +19,14 @@ header2 = ('FCM: FILM \n')
 footer1 = ('001  Bars   V     C        00:58:00:00 00:58:30:00 00:58:00:00 00:58:30:00\n')
 footer2 = ('* LOC: 00:58:20:00 green   Dummy clip that can be deleted\n')
 
-timeline = otio.adapters.read_from_file(inputEDL, ignore_timecode_mismatch=True)
-# ignore_timecode_mismatch=True deals with the source timecode and duration 
-# not matching the timebase.
-
 markerwriter = open(outputEDL, 'w')
 markerwriter.write(header1)
 markerwriter.write(header2)
 
 with open(inputEDL, newline = '') as openEDL:
-    #lines = csv.reader(markers, delimiter='\t')
     for line in openEDL:
         if '* LOC:' in line:
             print(line)
-#            name, timecode, layer, color, comment = line[:5]
-#            print('* LOC: {0} {1}   {2}'.format(timecode, color, comment))
-#            edl_line = ('* LOC: {0} {1}   {2}\n'.format(timecode, color, comment))
             markerwriter.write(line)
 
 markerwriter.write(footer1)
