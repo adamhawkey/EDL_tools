b19b31d4907eb100b86848acf43b4cbfe23be2d3
Adam S Hawkey <74074027+adamhawkey@users.noreply.github.com>
2022-02-08 15:57:51 -0800

    Add files via upload

---
+++ GT_prep/GT_Titles_0.2.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+usage: GT_Titles_0.1.py <inputEDL> <outputEDL>
+"""
+
+# This script is very specific to the show Good Trouble.
+# Incredibly specific to the titles EDL's that come from their editorial dept.
+
+import sys
+import shutil
+import os
+import re
+
+print(sys.argv[0])
+if len(sys.argv) < 2:
+    print('Not enough arguments.', __doc__)
+    sys.exit()
+
+inputEDL, outputEDL = sys.argv[1:]
+
+#shutil.copy(inputEDL, outputEDL)
+
+'''
+regex1 = (r"^([0-9]{3})([ ]+)(BL)(    V     C        )(00:00:00:00 00:00:00:00)[ ]+([0-9]{2}:[0-9]{2}:[0-9]{2}:[0-9]{2})(.[0-9]{2}:[0-9]{2}:[0-9]{2}:[0-9]{2})(.*)\n"
+	r"([0-9]{3})(.*)\n"
+	r"(\*)(.*)\n"
+	r"(\*)(.*)\n"
+	r"([0-9]{3})([ ]+BL    V     C)(.*)$\n"
+	r"(\*)(.*)\n"
+	r"([0-9]{3})(.*)\n"
+	r"([0-9]{3})([ ]+BL    V     D    012 )(00:00:00:00 00:00:00:12)[ ]+([0-9]{2}:[0-9]{2}:[0-9]{2}:[0-9]{2})( [0-9]{2}:[0-9]{2}:[0-9]{2}:[0-9]{2})(.*)$\n"
+	r"(.*)\n"
+	r"(.*)")
+'''
+regex = (r"^([0-9]{3})([ ]+)(BL)(.*)\n"
+	r"(\*)( SOURCE FILE: \(NULL\))")
+subst = ""
+
+regex1 = (r"^([0-9]{3})([ ]+)(BL)(    V     C        )(00:00:00:00 00:00:00:00)[ ]+([0-9]{2}:[0-9]{2}:[0-9]{2}:[0-9]{2})(.[0-9]{2}:[0-9]{2}:[0-9]{2}:[0-9]{2})(.*)\n"
+	r"([0-9]{3})(.*)\n"
+	r"(\*)(.*)\n"
+	r"(\*)(.*)\n"
+	r"(\*)(.*)\n"
+	r"([0-9]{3})([ ]+)([A-Za-z0-9_\-]+)( V     C        )(.*)$\n"
+	r"(\*)(.*)\n"
+	r"(\*)(.*)\n"
+	r"([0-9]{3})(.*)\n"
+	r"([0-9]{3})([ ]+BL    V     D    012 )(00:00:00:00 00:00:00:12)[ ]+([0-9]{2}:[0-9]{2}:[0-9]{2}:[0-9]{2})( [0-9]{2}:[0-9]{2}:[0-9]{2}:[0-9]{2})(.*)$\n"
+	r"(.*)\n"
+	r"(.*)\n"
+	r"(.*)")
+
+test_str = ("002     BL    V     C        00:00:00:00 00:00:00:00 00:03:13:16 00:03:13:16 \n"
+	"002  1_-_NAME V     D    012 01:00:00:00 01:00:00:12 00:03:13:16 00:03:14:04 \n"
+	"* BLEND, DISSOLVE \n"
+	"* TO CLIP NAME:  1 - NAME ONLY - RIGHT.PNG \n"
+	"* SOURCE FILE: (NULL)\n"
+	"003  1__NAME V     C        01:00:00:12 01:00:02:12 00:03:14:04 00:03:16:04 \n"
+	"* FROM CLIP NAME:  1 - NAME ONLY - RIGHT.PNG \n"
+	"* SOURCE FILE: 1 - NAME ONLY - RIGHT.PNG\n"
+	"004  1_-_NAME V     C        01:00:02:12 01:00:02:12 00:03:16:04 00:03:16:04 \n"
+	"004     BL    V     D    012 00:00:00:00 00:00:00:12 00:03:16:04 00:03:16:16 \n"
+	"* BLEND, DISSOLVE \n"
+	"* FROM CLIP NAME:  1 - NAME ONLY - RIGHT.PNG \n"
+	"* SOURCE FILE: 1 - NAME ONLY - RIGHT.PNG")
+
+subst1 = "\\17\\18\\19\\20\\5 \\6\\32"
+
+# You can manually specify the number of replacements by changing the 4th argument
+#result = re.sub(regex, subst, test_str, 0, re.MULTILINE)
+
+edlIn = open(inputEDL, "r")
+edl = edlIn.read()
+edlOut = open(outputEDL, "w")
+# You can manually specify the number of replacements by changing the 4th argument
+
+# delete standalone BL events:  https://regex101.com/r/kuafhd/1
+result = re.sub(regex, subst, edl, 0, re.MULTILINE)
+
+# replace dissolves with 1 clip:  https://regex101.com/r/VnSTI3/1
+result1 = re.sub(regex1, subst1, result, 0, re.MULTILINE)
+
+'''
+regex2 = (r"^([0-9]{3})([ ]+)(BL)(.*)")
+subst2 = "M2      GoodTrouble_4004_OT_R 0.0       00:00:00:01"
+
+regex3 = (r"00:00:00:00 00:00:00:00")
+subst3 = "00:00:00:01 00:00:00:01"
+
+regex4 = (r"* SOURCE FILE: (NULL)")
+subst4 = "\* FROM FILE: U:\\avid_jobs\\good-myr\\4004con\\05_titles\\open_titles\\GoodTrouble_4004_OT_R_000001.tif\n"
+'''
+
+if result1:
+    print(result1)
+    edlOut.write(result1)
+    edlOut.close()
---
+++ GT_prep/GT_prep_0.5.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+version: GT_prep_0.5.py
+"""
+
+# This script is very specific to the show Good Trouble.
+# Versioning up to 0.5 to split lutname on the _VEN delimiter.
+# Versioning up to 0.4 as we begin season 4 in November 2021.
+# Camera has changed to the Sony Venice X-OCN format in Venice_SLog3_SGamut3.cine space.
+#
+# In a future version, I may add variable amount of cc_layers, different colorspaces, and verbose/quiet switch
+
+import opentimelineio as otio
+import sys
+import re
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-i",
+    "--inEDL",
+    help="input EDL",
+    type=str
+    )
+parser.add_argument(
+    "-o",
+    "--outEDL",
+    help="output EDL",
+    type=str
+    )
+parser.add_argument(
+    "-v",
+    "--verbose",
+    action="store_true",
+    help="print each line",
+    )
+parser.add_argument(
+    "-q",
+    "--quiet",
+    action="store_true",
+    help="suppress terminal output",
+    )
+
+args = parser.parse_args()
+
+inputEDL = args.inEDL
+outputEDL = args.outEDL
+print("reading: ", inputEDL, "and writing: ", outputEDL)
+
+timeline = otio.adapters.read_from_file(inputEDL, ignore_timecode_mismatch=True)
+
+lut_path = ('T:\\luts\\GoodTrouble\\Season_4\\Season_04_SL3SG3C\\') # this is the local root path to the luts.
+lut_space = ('sl3_sg3c')
+lut_version = ('v3')
+lut_ext = ('cube')
+
+timeline = otio.adapters.read_from_file(inputEDL, ignore_timecode_mismatch=True)
+# ignore_timecode_mismatch=True deals with the source timecode and duration 
+# not matching the timebase.
+
+for clip in timeline.each_clip():
+    
+    edl_meta = clip.metadata.get('cmx_3600',{})
+    
+    nucoda_stack = [
+        'NUCODA_LAYER Log',
+        'NUCODA_LAYER Log2',
+        'NUCODA_LAYER Log3',
+        'NUCODA_LAYER Log4',
+        'NUCODA_LAYER Log5',
+        'NUCODA_LAYER Log6',
+    ]
+    
+    comments = edl_meta.get('comments', [])
+
+    for comment in comments:
+        if 'REEL: GT_' in comment:
+            _, lut_string = comment.split(": ")
+            lut_string_split = re.split(r'_VEN', lut_string)
+            lut_string_chars = re.split(r'[^A-Za-z0-9_]+', lut_string_split[0])
+            lut_root = lut_string_chars[0]
+            if args.verbose:
+                print('{0} becomes {1}_{2}.{4}'.format(lut_string, lut_root, lut_space, lut_version, lut_ext))
+            lut_layer = 'NUCODA_LAYER GT_LUT -effect NucodaCMSPath -lut {0}{1}_{2}.{4}'.format(lut_path, lut_root, lut_space, lut_version, lut_ext)
+            nucoda_stack.append(lut_layer)
+
+    comments.extend(nucoda_stack)
+
+otio.adapters.write_to_file(timeline, outputEDL, adapter_name='cmx_3600', style='nucoda')
+
